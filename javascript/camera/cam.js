9a44fa0b07cd70237f86c936425088c03add7ae3
Pasever <pas1034ever@gmail.com>
2017-12-27 20:48:12 -0500

    a.0.2

---
+++ javascript/camera/cam.js
@@ -0,0 +1,12 @@
+jQuery(function(){
+  
+  jQuery('#camera_wrap_4').camera({
+    height: 'auto',
+    loader: 'bar',
+    pagination: false,
+    thumbnails: false,
+    hover: false,
+    opacityOnGrid: false,
+    imagePath: 'images/'
+  });
+});
